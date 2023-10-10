3a2ec6826a6162ed041f1bb731cc63a0fc5d8f5f
Serene Leyba <development@softwavestudios.com>
2023-10-10 13:43:48 -0400

    Added my new emacs config

---
+++ doom/config.el
@@ -0,0 +1,138 @@
+(global-set-key (kbd "M-9") 'kill-whole-line)
+
+(after! core-ui (menu-bar-mode 1))
+(menu-bar-mode 1)
+(tool-bar-mode 1)
+(scroll-bar-mode 1)
+(cua-mode t)
+
+(add-hook! 'doom-load-theme-hook :append
+  (setq doom-font (font-spec :family "Mx437 IBM BIOS-2y" :size 16))
+  (doom/reload-font))
+
+(setq doom-theme 'Spaceworm92)
+
+;(add-to-list 'load-path "/home/discovery/.config/doom/copilot")
+(require 'copilot)
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+
+
+(setq ;; makes killing/yanking interact with the clipboard
+      x-select-enable-clipboard t
+
+      ;; I'm actually not sure what this does but it's recommended?
+      x-select-enable-primary t
+
+      ;; Save clipboard strings into kill ring before replacing them.
+      ;; When one selects something in another program to paste it into Emacs,
+      ;; but kills something in Emacs before actually pasting it,
+      ;; this selection is gone unless this variable is non-nil
+      save-interprogram-paste-before-kill t
+
+      ;; Shows all options when running apropos. For more info,
+      ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
+      apropos-do-all t
+
+      ;; Mouse yank commands yank at point instead of at click.
+      mouse-yank-at-point t)
+
+;; Load cheatsheet
+(defun my-load-cheat ()
+  "Load and display the cheatsheet"
+  (interactive)
+  (find-file "/home/discovery/Documents/TheVault/Cheatsheets/Emacs.md"))
+(global-set-key (kbd "C-x M-c") 'my-load-cheat)
+;; Add menu item to tools menu
+(define-key-after
+  global-map
+  [menu-bar tools my-load-cheat]
+  '("Load Cheatsheet" . my-load-cheat)
+  'compile)
+
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;;(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+
+(nyan-mode)
+(nyan-toggle-wavy-trail)
+;;(setq nyan-wavy-trail t nyan-minimum-window-width 1 nyan-bar-length 12)
+(nyan-start-animation)
+(use-package! whitespace
+  :config
+  (setq
+    global-whitespace-mode nil))
---
+++ doom/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("38c0c668d8ac3841cb9608522ca116067177c92feeabc6f002a27249976d7434" "9013233028d9798f901e5e8efb31841c24c12444d3b6e92580080505d56fd392" "9dccdccfeb236623d5c7cf0250a92308cf307afde4ebdaf173b59e8bbbae1828" "6f96a9ece5fdd0d3e04daea6aa63e13be26b48717820aa7b5889c602764cf23a" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "0d0936adf23bba16569c73876991168d0aed969d1e095c3f68d61f87dd7bab9a" "d6a75e11f1d73251881ba3b183d58138ab964b63eb135ecc4c49f68792622d0d" default))
+ '(ignored-local-variable-values
+   '((projectile-project-compilation-cmd . "msbuild /home/discovery/Documents/Dev/GodotProjects/StarFlyer2")
+     (projectile-project-configure-cmd . "/home/discovery/Documents/GodotMono_3.5.2/Godot_v3.5.2-stable_mono_x11.64 --path \"/home/discovery/Documents/Dev/GodotProjects/StarFlyer2\" --editor")
+     (projectile-project-run-cmd . "/home/discovery/Documents/GodotMono_3.5.2/Godot_v3.5.2-stable_mono_x11.64 --path \"/home/discovery/Documents/Dev/GodotProjects/StarFlyer2\"")))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(nyan-mode))
+ '(warning-suppress-log-types '((lsp-mode) (lsp-mode) (defvaralias)))
+ '(warning-suppress-types '((lsp-mode) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
